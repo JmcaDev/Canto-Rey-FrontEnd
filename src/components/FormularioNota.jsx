98be3d77f2c867ddae140846f9b2ca6755f78eab
JmcaDev <jmcavzla@gmail.com>
2023-12-05 10:17:04 -0400

    inicio moduloNotas

--- src/components/FormularioNota.jsx
+++ src/components/FormularioNota.jsx
@@ -1,12 +1,32 @@
-
+import useNotas from "../hooks/useNotas"
+import { useState } from "react"
 
 function FormularioNota() {
+
+  const {clientes } = useNotas()
+
+  const [clienteSeleccionado, setClienteSeleccionado] = useState({})
+
   return (
     <form className="bg-white py-10 px-5 md:w-1/2 rounded-lg">
         <div>
-            <label htmlFor="" className="text-gray-700 uppercase font-bold text-sm">
-
-            </label>
+            <label htmlFor="" 
+              className="text-gray-700 uppercase font-bold text-sm"
+            >Cliente</label>
+            <select 
+              name="cliente" 
+              id="cliente"
+              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value={clienteSeleccionado}
+              onChange={e => setClienteSeleccionado(e.target.value)}
+            >
+              {clientes.length ? 
+                (clientes.map(cliente => (
+                  <option value={cliente._id} key={cliente._id}>{cliente.nombre}</option>
+                )))
+                : <option value="">No hay registros</option>
+              }
+            </select>
         </div>
     </form>
   )
